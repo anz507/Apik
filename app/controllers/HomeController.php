1b353c093bddd638e34b13da2b1f1e55b0c089ab
Ahmad <ahmad@dominopos.com>
2017-03-04 02:45:08 +0800

    Add new route for unspash api

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,22 +2,47 @@
 
 class HomeController extends BaseController {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Default Home Controller
+    |--------------------------------------------------------------------------
+    |
+    | You may wish to use controllers instead of, or in addition to, Closure
+    | based routes. That's great! Here is an example controller method to
+    | get you started. To route to this controller, just add the route:
+    |
+    |   Route::get('/', 'HomeController@showWelcome');
+    |
+    */
 
-	public function showWelcome()
-	{
-		return View::make('hello');
-	}
+    public function showWelcome()
+    {
+        return View::make('hello');
+    }
+
+    public function postLine()
+    {
+        $img = $this->getCurl('https://source.unsplash.com/random');
+
+        return $img;
+    }
+
+    protected function getCurl($uri, $flag = false)
+    {
+        $output = "";
+        try {
+            $ch = curl_init($uri);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 4);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $flag);
+            $output = curl_exec($ch);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        return $output;
+    }
 
 }
