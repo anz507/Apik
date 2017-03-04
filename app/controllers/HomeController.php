3bde319aed2f0c2da985a02fbd39152e6beec47c
Ahmad <ahmad@dominopos.com>
2017-03-04 15:07:00 +0800

    Create method to get random image and search by keyword and also create line webhooks controller

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,5 +1,14 @@
 <?php
 
+use \LINE\LINEBot;
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot\Constant\HTTPHeader;
+use \LINE\LINEBot\Event\MessageEvent;
+use \LINE\LINEBot\Event\MessageEvent\TextMessage;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+
 class HomeController extends BaseController {
 
     /*
@@ -22,9 +31,117 @@ class HomeController extends BaseController {
 
     public function postLine()
     {
-        $img = $this->getCurl('https://source.unsplash.com/random');
+        $httpClient = new CurlHTTPClient('w3m29w+IVtbYWnZu+aXcnNlCJuC0gEBfaRNHBzRIAScxxwi4QEUpApX8Puiy8F83AET3nuf9KElWOIwX8nYkNuuX7W41ZEp6OHYkqMTG5qskpTlrUrdkb5/czGmKM0dvspOxqlLjV7ZwsTYuuA9vzgdB04t89/1O/w1cDnyilFU=');
+        $bot = new LINEBot($httpClient, ['channelSecret' => 'ada22e4446381a3c79460884c6fa2e96']);
 
-        return $img;
+        $signature = Request::header(HTTPHeader::LINE_SIGNATURE);
+
+        Log::info('[LINE] Info signature 1: ' . $signature);
+
+        if (empty($signature)) {
+            Log::error('[LINE] Invalid signature 2');
+            $response = Response::make('Bad Request', 400);
+            return $response;
+        }
+
+        // Check request with signature and parse request
+        try {
+            $request = Request::instance();
+            $requestBody = $request->getContent();
+            $events = $bot->parseEventRequest($requestBody, $signature);
+
+            foreach ($events as $event) {
+                if (!($event instanceof MessageEvent)) {
+                    Log::info('[LINE] Text Message Come');
+                    continue;
+                }
+                if (!($event instanceof TextMessage)) {
+                    Log::info('[LINE] Non Text Message Come');
+                    continue;
+                }
+
+                Log::info('[LINE] Reply from User: ' . $event->getText());
+
+                $data = new stdclass();
+                switch ($event->getText()) {
+                    case 'random':
+                        $data = $this->getRandomImage();
+                        break;
+
+                    default:
+                        $data = $this->getImageByKeyword($event->getText());
+                        break;
+                }
+
+                $TextMessageBuilder = new TextMessageBuilder($data->author);
+                $ImageMessageBuilder = new ImageMessageBuilder($data->url, $data->thumb);
+                $MultiMessageBuilder = new MultiMessageBuilder();
+                $MultiMessageBuilder->add($TextMessageBuilder);
+                $MultiMessageBuilder->add($ImageMessageBuilder);
+                $response = $bot->replyMessage($event->getReplyToken(), $MultiMessageBuilder);
+
+                Log::info('[LINE] LINEBot Response: ' . serialize($response));
+            }
+
+            if ($response->isSucceeded()) {
+                $response = Response::make('Success', 200);
+                return $response;
+            }
+        } catch (\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+            Log::error('[LINE] Invalid signature');
+            return $res->withStatus(400, 'Invalid signature');
+        } catch (\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+            Log::error('[LINE] Unknown event type has come');
+            return $res->withStatus(400, 'Unknown event type has come');
+        } catch (\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+            Log::error('[LINE] Unknown message type has come');
+            return $res->withStatus(400, 'Unknown message type has come');
+        } catch (\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+            Log::error('[LINE] Invalid event request');
+            return $res->withStatus(400, "Invalid event request");
+        } catch (Exception $e) {
+            Log::error('[LINE] General Error: ' . $e->getMessage());
+        }
+    }
+
+    public function getRandomImage()
+    {
+        $url = 'https://api.unsplash.com/photos/random';
+        $img = json_decode($this->getCurl($url));
+
+        $data = new stdclass();
+
+        if (is_object($img)) {
+            $imgUrl = $img->urls->regular;
+            $imgThumbUrl = $img->urls->thumb;
+            $imgAuthor = $img->user->name;
+            $data->url = $imgUrl;
+            $data->author = $imgAuthor;
+            $data->thumb = $imgThumbUrl;
+        }
+
+        return $data;
+    }
+
+    public function getImageByKeyword($keyword = '')
+    {
+        $keyword = empty($keyword) ? Input::get('keyword') : $keyword;
+        $url = 'https://api.unsplash.com/search/photos?query=' . $keyword;
+        $imgs = json_decode($this->getCurl($url));
+
+        $data = new stdclass();
+        if (count($imgs->results) > 0) {
+            $randImgKey = array_rand($imgs->results);
+            $img = $imgs->results[$randImgKey];
+            $imgUrl = $img->urls->regular;
+            $imgThumbUrl = $img->urls->thumb;
+            $imgAuthor = $img->user->name;
+            $data->url = $imgUrl;
+            $data->author = $imgAuthor;
+            $data->thumb = $imgThumbUrl;
+        }
+
+        return $data;
     }
 
     protected function getCurl($uri, $flag = false)
@@ -32,6 +149,9 @@ class HomeController extends BaseController {
         $output = "";
         try {
             $ch = curl_init($uri);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Authorization: Bearer 8045aedab8b74c5a7f0fae2b04fddc086484349d897682f86b5686a79dde58ef'
+            ));
             curl_setopt($ch, CURLOPT_HEADER, 0);
             curl_setopt($ch, CURLOPT_TIMEOUT, 4);
             curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
